ff63b33b430c1555183ae65511039f2cce9d1acb
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-10 12:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,5 +22,5 @@ function firstNonRepeatedChar(str) {
               }
                
    }
-   const input = prompt("Enter a string");
+   const input = prompt("Enter a string")
    alert(firstNonRepeatedChar(input)); 
