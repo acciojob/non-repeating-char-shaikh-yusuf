7b25199354d98c6303679df195ef4f23d71aa707
shaikh-yusuf <94888066+shaikh-yusuf@users.noreply.github.com>
2023-09-11 21:20:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
 function firstNonRepeatedChar(str) {
     // Write your code here
     let i=0;
-	
           while(i<=str.length)
               { 
-				   let scor=0
                 let j=0;
-				  
                    while(j<=str.length)
-                       {   
-						  
-                           if(str.charAt(i)===str.charAt(j))
+                       {
+                           if(str.charAt(i)!=str.charAt(j))
                            {
-                              score++
+                              return str.charAt(i);
+                           }else
+                           {
+                            return null;
                            }
                            
                            j++;  
                        }
-				  
                      
-          if(score<2){
-		return str.str.charAt(i)
-		  }else{
-			  return null
-		  }
+
                        i++;
               }
-	
-	}
                
    }
-   const input = prompt("Enter a string");
-
+   // const input = prompt("Enter a string")
    alert(firstNonRepeatedChar(input)); 
